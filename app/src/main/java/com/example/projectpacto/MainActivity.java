78ef347250a3b8dab33e131fa985755fbdbcdc9a
fajrulalam <71478225+fajrulalam@users.noreply.github.com>
2022-03-06 15:25:11 +0700

    adding resources for the real main activity

--- app/src/main/java/com/example/projectpacto/MainActivity.java
+++ app/src/main/java/com/example/projectpacto/MainActivity.java
@@ -1,15 +1,58 @@
 package com.example.projectpacto;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.projectpacto.databinding.ActivityMainBinding;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+//    TextInputEditText emailEditText;
+//    TextInputEditText passwordEditText;
+//    TextView lupaPassword;
+//    Button loginButton;
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+
+
+        binding.masukButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = String.valueOf(binding.emailEditText.getText());
+                String password = binding.passwordEditText.getText().toString();
+                if (email.matches("admin") && password.matches("password123")){
+                    Toast.makeText(getApplicationContext(), "Login Succeessful", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+
+                }
+                Log.i(TAG, "onClick: Email is " + email);
+                Log.i(TAG, "onClick: Password is " + password);
+            }
+        });
+
+
+
 
     }
 }
