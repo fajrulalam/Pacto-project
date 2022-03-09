b79081201a6a09b723fd0f6532da940f0d52dcf5
fajrulalam <71478225+fajrulalam@users.noreply.github.com>
2022-03-09 20:17:12 +0700

    bottom sheet can now show, almost functional

--- app/src/main/java/com/example/projectpacto/PenumpangBottomSheet.java
+++ app/src/main/java/com/example/projectpacto/PenumpangBottomSheet.java
@@ -1,64 +1,132 @@
 package com.example.projectpacto;
 
+import android.app.Dialog;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.example.projectpacto.databinding.FragmentPenumpangBottomSheetBinding;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PenumpangBottomSheet#newInstance} factory method to
- * create an instance of this fragment.
+
  */
-public class PenumpangBottomSheet extends Fragment {
+public class PenumpangBottomSheet extends BottomSheetDialogFragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentPenumpangBottomSheetBinding binding;
+    ImageView minusDewasa;
+    ImageView plusDewasa;
+    ImageView minusAnak;
+    ImageView plusAnak;
+    ImageView minusBalita;
+    ImageView plusBalita;
+    RadioGroup radioGroup;
+    RadioButton radioButton1;
+    RadioButton radioButton2;
+    RadioButton radioButton3;
+    RadioButton radioButton4;
+    Button actionButton;
 
     public PenumpangBottomSheet() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PenumpangBottomSheet.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PenumpangBottomSheet newInstance(String param1, String param2) {
-        PenumpangBottomSheet fragment = new PenumpangBottomSheet();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final BottomSheetDialog dialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
+        final View view = View.inflate(getContext(), R.layout.fragment_penumpang_bottom_sheet, null);
+        dialog.setContentView(view);
+
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from((View) view.getParent());
+        bottomSheetBehavior.setPeekHeight(BottomSheetBehavior.PEEK_HEIGHT_AUTO);
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (BottomSheetBehavior.STATE_EXPANDED == newState) {
+//                    showView(appBarLayout, getActionBarSize());
+                    hideView(view.findViewById(R.id.linearLayout));
+                }
+
+                if (BottomSheetBehavior.STATE_COLLAPSED == newState)   {
+//                    hideView(appBarLayout);
+                    showView(view.findViewById(R.id.linearLayout), getActionBarSize());
+                }
+
+                if (BottomSheetBehavior.STATE_HIDDEN == newState) {
+                    dismiss();
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+        view.findViewById(R.id.closeSheet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+
+
+
+
+    return dialog;
+
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+    private void hideView(View view) {
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        params.height = 0;
+        view.setLayoutParams(params);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_penumpang_bottom_sheet, container, false);
+    private void showView(View view, int size) {
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        params.height = size;
+        view.setLayoutParams(params);
     }
+
+    private int getActionBarSize(){
+        final TypedArray typedArray =getContext().getTheme().obtainStyledAttributes(new int[]{
+                android.R.attr.actionBarSize
+        });
+
+        return (int) typedArray.getDimension(0, 0);
+    }
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+
+
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        binding = FragmentPenumpangBottomSheetBinding.inflate(())
+////        return inflater.inflate(R.layout.fragment_penumpang_bottom_sheet, container, false);
+//    }
 }
--- app/src/main/java/com/example/projectpacto/PlaneOrderActivity1.java
+++ app/src/main/java/com/example/projectpacto/PlaneOrderActivity1.java
@@ -3,10 +3,12 @@ package com.example.projectpacto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.projectpacto.databinding.ActivityPlaneOrder1Binding;
+import com.example.projectpacto.databinding.FragmentPenumpangBottomSheetBinding;
 import com.google.android.material.datepicker.CalendarConstraints;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
@@ -21,6 +23,30 @@ public class PlaneOrderActivity1 extends AppCompatActivity {
         binding = ActivityPlaneOrder1Binding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        binding.tanggalKeberangkatTextInput.getEditText().setInputType(TextView.AUTO_SIZE_TEXT_TYPE_NONE);
+        binding.penumpangTextInput.getEditText().setInputType(TextView.AUTO_SIZE_TEXT_TYPE_NONE);
+
+
+        //Penumpang
+        binding.penumpangTextInput.getEditText().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("CLICK LISTENED", "It's clicked!");
+                PenumpangBottomSheet penumpangBottomSheet = new PenumpangBottomSheet();
+                penumpangBottomSheet.show(getSupportFragmentManager(), penumpangBottomSheet.getTag());
+            }
+        });
+        binding.penumpangTextInput.getEditText().setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b==true) {
+                    Log.i("CLICK LISTENED", "It's focused...");
+                    PenumpangBottomSheet penumpangBottomSheet = new PenumpangBottomSheet();
+                    penumpangBottomSheet.show(getSupportFragmentManager(), penumpangBottomSheet.getTag());
+
+                }
+            }
+        });
 
         //Calendar Constraints
         long today = MaterialDatePicker.todayInUtcMilliseconds();
@@ -31,7 +57,6 @@ public class PlaneOrderActivity1 extends AppCompatActivity {
                 .setCalendarConstraints(constraints.build())
                 .setTitleText("Pilih Tanggal Keberangkatan").build();
 
-        binding.tanggalKeberangkatTextInput.getEditText().setInputType(TextView.AUTO_SIZE_TEXT_TYPE_NONE);
 
 
         binding.tanggalKeberangkatTextInput.getEditText().setOnClickListener(new View.OnClickListener() {
