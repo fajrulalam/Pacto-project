0b2668e0aa06d2d821fdd6202f298527f1f85e70
fajrulalam <71478225+fajrulalam@users.noreply.github.com>
2022-03-22 23:09:57 +0700

    hotel_options_single_view.xml is done

---
+++ app/src/main/java/com/example/projectpacto/HotelOrderActivity2.java
@@ -0,0 +1,14 @@
+package com.example.projectpacto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HotelOrderActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel_order2);
+    }
+}
