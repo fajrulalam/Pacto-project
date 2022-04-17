f8eebdab271eec48ab086c8de9547451fe466ad9
fajrulalam <71478225+fajrulalam@users.noreply.github.com>
2022-04-17 20:16:56 +0700

    filter transaction ui done

---
+++ app/src/main/java/com/example/projectpacto/TransaksiFilterButtonSheet.java
@@ -0,0 +1,33 @@
+package com.example.projectpacto;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+
+public class TransaksiFilterButtonSheet extends BottomSheetDialogFragment {
+
+    public TransaksiFilterButtonSheet() {
+        // Required empty public constructor
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+
+
+
+
+}
